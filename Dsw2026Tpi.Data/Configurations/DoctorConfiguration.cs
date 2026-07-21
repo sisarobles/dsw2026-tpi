73af11cd23a9d6b364484b4bc248b87c3c2dc5be
Maia <maiafedericag@gmail.com>
2026-07-21 15:25:38 -0300

    se aplico la migracion para las validaciones de doctor y especialidad

--- Dsw2026Tpi.Data/Configurations/DoctorConfiguration.cs
+++ Dsw2026Tpi.Data/Configurations/DoctorConfiguration.cs
@@ -9,5 +9,21 @@ public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
     public void Configure(EntityTypeBuilder<Doctor> builder)
     {
         builder.ToTable("Doctors");
+
+        // 2. Validaciones para el Nombre
+        builder.Property(d => d.Name)
+               .IsRequired()             
+               .HasMaxLength(100);      
+
+        // 3. Validaciones para la Matrícula (LicenseNumber)
+        builder.Property(d => d.LicenseNumber)
+               .IsRequired()
+               .HasMaxLength(50);        
+
+        // 4. Configura la Relación (Foreign Key)
+        builder.HasOne(d => d.Speciality)
+               .WithMany()
+               .HasForeignKey(d => d.SpecialityId)
+               .OnDelete(DeleteBehavior.Restrict); 
     }
 }
