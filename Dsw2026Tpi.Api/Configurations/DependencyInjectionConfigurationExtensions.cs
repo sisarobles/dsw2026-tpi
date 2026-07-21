3bf3519bbd435591c5ae1fe95df8ba54d2850779
Jazmin Herrera <jazminherrera1171@gmail.com>
2026-07-21 18:54:28 -0300

    diseñe la interfaz de servicio

--- Dsw2026Tpi.Api/Configurations/DependencyInjectionConfigurationExtensions.cs
+++ Dsw2026Tpi.Api/Configurations/DependencyInjectionConfigurationExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Dsw2026Tpi.Api.Services;
 using Dsw2026Tpi.Application.Interfaces;
 using Dsw2026Tpi.Application.Services;
+using Dsw2026Tpi.CrossCutting.Logging;
 using Dsw2026Tpi.Data;
 using Dsw2026Tpi.Domain.Interfaces;
 
@@ -15,6 +16,7 @@ public static class DependencyInjectionConfigurationExtensions
         services.AddScoped<IAuthenticationService, AuthenticationService>();
         services.AddScoped<ISignInService, SignInService>();
         services.AddSingleton<JwtService>();
+        services.AddScoped<ILogService, LogService>();
         return services;
     }
 }
