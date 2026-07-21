cef41aa43eb8122f67495e0b23effdf040c5280f
Maia <maiafedericag@gmail.com>
2026-07-21 16:48:39 -0300

    se armo los servicios (Capa de Aplicacion)

---
+++ Dsw2026Tpi.Application/Interfaces/ISpecialityService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Interfaces
+{
+    internal interface ISpecialityService
+    {
+    }
+}
---
+++ Dsw2026Tpi.Application/Services/SpecialityService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Services
+{
+    internal class SpecialityService
+    {
+    }
+}
