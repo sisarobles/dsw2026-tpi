e3343835d46f1899ff46df7029011a341efef9b0
Maia <maiafedericag@gmail.com>
2026-07-21 15:47:14 -0300

    se armo los Dtos (Capa de Aplicacion)

---
+++ Dsw2026Tpi.Application/Dtos/Doctors/CreateDoctorDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Dtos.Doctors
+{
+    internal class CreateDoctorDto
+    {
+    }
+}
---
+++ Dsw2026Tpi.Application/Dtos/Specialities/UpdateSpecialityDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Application.Dtos.Specialities
+{
+    internal class UpdateSpecialityDto
+    {
+    }
+}
