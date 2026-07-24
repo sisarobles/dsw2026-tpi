0fd6867fb6c5118f319b52b9d2d00bee239c395c
Jazmin Herrera <jazminherrera1171@gmail.com>
2026-07-24 12:27:48 -0300

    agregar la entidad patient

--- Dsw2026Tpi.Domain/Entities/Patient.cs
+++ Dsw2026Tpi.Domain/Entities/Patient.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Dsw2026Tpi.Domain.Entities;
 
-namespace Dsw2026Tpi.Domain.Entities
+public class Patient : EntityBase
 {
-    internal class Patient
+    public string UserId { get; init; }
+    public long Dni { get; init; }
+    public string? FullName { get; private set; }
+    public bool IsActive { get; private set; }
+
+    #region Constructor for EF
+#pragma warning disable CS8618
+    private Patient()
     {
+    }
+#pragma warning restore CS8618
+    #endregion
+
+    public Patient(string userId, long dni, string? fullName = null, Guid? id = null) : base(id)
+    {
+        UserId = userId;
+        Dni = dni;
+        FullName = fullName;
+        IsActive = true;
+    }
+
+    public void UpdateFullName(string fullName)
+    {
+        FullName = fullName;
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
     }
 }
