b45cfe93cd3a65ac4ed8f752cadaccd1ec859edb
Jazmin Herrera <jazminherrera1171@gmail.com>
2026-07-23 21:00:37 -0300

    agregar la entidad patient

---
+++ Dsw2026Tpi.Data/Configurations/PatientConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations;
+
+public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+{
+    public void Configure(EntityTypeBuilder<Patient> builder)
+    {
+        builder.ToTable("Patients");
+
+        builder.HasIndex(p => p.Dni)
+            .IsUnique();
+
+        builder.HasIndex(p => p.UserId)
+            .IsUnique();
+    }
+}
